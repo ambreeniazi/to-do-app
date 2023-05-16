db1f53dee3d33c42287ca73070b1ee39815ed8f7
Ambreen786928 <129070705+Ambreen786928@users.noreply.github.com>
2023-05-16 16:38:48 +0500

    Validation

--- src/App.js
+++ src/App.js
@@ -1,67 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import Todo from './components/Todo';
+import React from 'react'
+import "./App.css";
+import Todo from "./components/Todo.jsx";
 
-const App = () => {
-  const [task ,settask] =useState( ' ');
-  const [mytask, setmytask]= useState(' ');
-
-//   useEffect(()=>{
-     
-//   })
-
-
-
-//   function submit(){
-//   const items = [
-//     id ="mytask.length+1",
-//     task= "mytask",
-//     complete="false",
-//     notified='false'
-//   ]};
-//   const newTodos = mytask.slice();
-//   newTodos.push(items);
-//   setmytask(newTodos);
-//   localStorage.setItems("items",JSON.stringify(newTodos));
-//   setmytask( ' ');
-// }
-
-
-
-
+function App() {
   return (
-    <>
-    
-    <h1>To-Do-App</h1>
-    <p>Filter-Tasks</p>
-    <div className=''><Todo/>
-     <form className='#'>
-    <input type='text'  id="Text1" className='text1' placeholder="Add Task"></input>
-    <button type='submit' value="submit" className='submit' placeholder='Button'>Add</button>
-     <span><p>  
-        <select name="#" id="#" required>
-            <option value="">--Select an option--</option>
-            <option value="Pending">Pending</option>
-            <option value="Progress">Progress</option>
-            <option value="Done">Done</option>
-        </select>
-      </p></span>
-      </form>
+    <div>
+      <Todo/>
     </div>
-
-    {/* < div id ="addtask" className=''/>
-    {
-      setmytask.length!=0 && mytask.map((items,index)
-      => {
-        return(
-          < Todo items={items}
-          key={index}
-          index={index}
-          remove={remove}
-          checked={checked}/>)})}
-         */}</>
-      )
-
-        }
+  )
+}
 
 export default App
 
